1e37992ba9c86e6894c43c89d555b022ceb3c5c0
keldykins <kelcundy@gmail.com>
2020-11-07 12:58:44 -0700

    made functional

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# 9-readme-generator
+# README Generator
+
+## Description
+
+README Generator is used to generate README.md files for projects. Using the Node inquirer package.
+
+## Installation
+
+Open README Generator folder in terminal and install using `npm install`, `npm start` to prompt questions which answers will then be used to generate a README file in your projects directory.
+
+## Usage
+
+Prompt questions in terminal, answer them, generate file.
+
+## Contact
+
+Github: [keldykins](http://github.com/keldykins)
+<br />
+Email: [kelcundy@gmail.com](kelcundy@gmail.com)
+
+## License
+
+Copyright © 2020 Keldan Cundy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- index.js
+++ index.js
@@ -4,130 +4,73 @@ const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-function promptUser() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "title",
-      message: "What is your project title?",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "What is the description of your project?",
-    },
-    {
-      type: "input",
-      name: "tableofcontents",
-      message: "What is your table of contents?",
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "What is the installation of your project?",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "Enter the projects usage",
-    },
-    {
-      type: "input",
-      name: "license",
-      message: "Enter the projects license",
-    },
-    {
-      type: "input",
-      name: "contributing",
-      message: "Enter the contributors of the project",
-    },
-    {
-      type: "input",
-      name: "tests",
-      message: "Enter the projects tests",
-    },
-    {
-      type: "input",
-      name: "License",
-      message: "Enter the license information",
-    },
-  ]);
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the description of your project?",
+  },
+  {
+    type: "input",
+    name: "tableofcontents",
+    message: "What is your table of contents?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What is the installation of your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter the projects usage",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Enter the contributors of the project",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Enter the projects tests",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "Enter the license information",
+  },
+];
+
+function promptUser(array) {
+  return inquirer.prompt(array);
 }
 
-function generateMD(answers) {
+function generateMarkdown(results) {
   return `
-  # ${answers.title}
-
-${answers.license}
+  # ${results.title}
+## ${results.description}
+## ${results.tableofcontents}
+## ${results.license}
+## ${results.contributing}
+## ${results.tests}
   ![GitHub repo size](https://img.shields.io/github/repo-size/scottydocs/README-template.md)
-  
-## ${answers.description}
-
-  
-  ## Installing <project_name>
-  
-  To install <project_name>, follow these steps:
-  
-  Linux and macOS:
-  ```
-  <install_command>
-  ```
-  
-  Windows:
-  ```
-  <install_command>
-  ```
-  ## Using <project_name>
-  
-  To use <project_name>, follow these steps:
-  
-  ```
-  <usage_example>
-  ```
-  
-  Add run commands and examples you think users will find useful. Provide an options reference for bonus points!
-  
-  ## Contributing to <project_name>
-  <!--- If your README is long or you have some specific process or steps you want contributors to follow, consider creating a separate CONTRIBUTING.md file--->
-  To contribute to <project_name>, follow these steps:
-  
-  1. Fork this repository.
-  2. Create a branch: `git checkout -b <branch_name>`.
-  3. Make your changes and commit them: `git commit -m '<commit_message>'`
-  4. Push to the original branch: `git push origin <project_name>/<location>`
-  5. Create the pull request.
-  
-  Alternatively see the GitHub documentation on [creating a pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
-  
-  ## Contributors
-  
-  Thanks to the following people who have contributed to this project:
-  
-  * [@scottydocs](https://github.com/scottydocs) 📖
-  * [@cainwatson](https://github.com/cainwatson) 🐛
-  * [@calchuchesta](https://github.com/calchuchesta) 🐛
-  
-  You might want to consider using something like the [All Contributors](https://github.com/all-contributors/all-contributors) specification and its [emoji key](https://allcontributors.org/docs/en/emoji-key).
-  
-  ## Contact
-  
-  If you want to contact me you can reach me at <your_email@address.com>.
-  
-  ## License
-  <!--- If you're not sure which open license to use see https://choosealicense.com/--->
-  
-  This project uses the following license: [<license_name>](<link>).
 `;
 }
 
-promptUser()
+promptUser(questions)
   .then(function (answers) {
-    const html = generateHTML(answers);
+    const md = generateMarkdown(answers);
 
-    return writeFileAsync("index.html", html);
+    return writeFileAsync("README.md", md);
   })
   .then(function () {
-    console.log("Successfully wrote to index.html");
+    console.log("Successfully wrote to README.md");
   })
   .catch(function (err) {
     console.log(err);
